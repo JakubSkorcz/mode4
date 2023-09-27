29e5d0407b8455c572659aee45fe5d4d7ee510fa
JakSko <j.skorcz@aniro-engineering.pl>
2023-09-27 09:22:45 +0200

    firstcommit

--- src/Mod3Zad1.java
+++ src/Mod3Zad1.java
@@ -1,10 +1,10 @@
-import org.jetbrains.annotations.NotNull;
+
 
 public class Mod3Zad1 {
     public int sumArray(int[] numbers){
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++){
-            sum = sum + numbers[i];
+        for (int number : numbers) {
+            sum = sum + number;
         }
         return sum;
     }
